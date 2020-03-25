9b87f9c9da9c0f45685ac821a5920dde7a3ff51a
izayoiriko <mmoursnuimneg@gmail.com>
2020-03-25 21:21:03 +0000

    3/26 header_on&off 追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcace)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "認証失敗"
+      render :new
+    end
+  end
+end
